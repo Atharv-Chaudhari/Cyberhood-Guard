94b58f5c2359828d0bc50892f019c0cb461f76cb
Atharv Chaudhari <61587515+Atharv-Chaudhari@users.noreply.github.com>
2022-07-03 16:34:14 +0530

    Add files via upload

---
+++ pycodes/caesar.py
@@ -0,0 +1,21 @@
+def caesar_encrypt(x="Hello",key=4):
+    y=""
+    for i in x:
+        if(i.isupper()):
+            y=y+chr((ord(i)+key-65)%26+65)
+        elif(i.islower()):
+            y=y+chr((ord(i)+key-97)%26+97)
+        else:
+            y=y+i
+    return y
+
+def caesar_decrypt(x,key=4):
+    y=""
+    for i in x:
+        if(i.isupper()):
+            y=y+chr((ord(i)+26-key-65)%26+65)
+        elif(i.islower()):
+            y=y+chr((ord(i)+26-key-97)%26+97)
+        else:
+            y=y+i
+    return y
