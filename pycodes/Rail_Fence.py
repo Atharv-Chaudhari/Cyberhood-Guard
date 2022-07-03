389e9cb220faa022a13749041924a136e7159ce1
Atharv Chaudhari <61587515+Atharv-Chaudhari@users.noreply.github.com>
2022-07-03 22:35:02 +0530

    Add files via upload

--- pycodes/Rail_Fence.py
+++ pycodes/Rail_Fence.py
@@ -1,6 +1,6 @@
 def Rail_Fence_encrypt(text="Hello",rails=3):
     l=len(text)
-    mat=[["-" for i in range(l)] for j in range(rails)]
+    mat=[["^" for i in range(l)] for j in range(rails)]
     i,j=0,0
     c=False
     for k in text:
@@ -15,45 +15,38 @@ def Rail_Fence_encrypt(text="Hello",rails=3):
     ans=""
     for i in mat:
         ans=ans+"".join(i)
-    ans=ans.replace("-","")
+    ans=ans.replace("^","")
     return ans
 
 def Rail_Fence_decrypt(cipher, key=3):
-    rail = [['\n' for i in range(len(cipher))]
-                  for j in range(key)]
-    dir_down = None
-    row, col = 0, 0
-    for i in range(len(cipher)):
-        if row == 0:
-            dir_down = True
-        if row == key - 1:
-            dir_down = False
-        rail[row][col] = '-'
-        col += 1
-        if dir_down:
-            row += 1
+    ans=""
+    l=[['^' for i in range(len(cipher))] for j in range(key)]
+    z=0
+    m=0
+    while(z<key):
+        i,j=0,0
+        c=False
+        for k in range(len(cipher)):
+            if(i==0 or i==key-1):
+                c=not c
+            if(z==i):
+                l[i][j]=cipher[m]
+                m=m+1
+            j=j+1
+            if(c):
+                i=i+1
+            else:
+                i=i-1
+        z=z+1
+    i,j=0,0
+    c=False
+    for k in cipher:
+        if(i==0 or i==key-1):
+            c=not c
+        ans=ans+l[i][j]
+        j=j+1
+        if(c):
+            i=i+1
         else:
-            row -= 1
-    index = 0
-    for i in range(key):
-        for j in range(len(cipher)):
-            if ((rail[i][j] == '-') and
-               (index < len(cipher))):
-                rail[i][j] = cipher[index]
-                index += 1
-    result = []
-    row, col = 0, 0
-    for i in range(len(cipher)):
-        if row == 0:
-            dir_down = True
-        if row == key-1:
-            dir_down = False
-        if (rail[row][col] != '-'):
-            result.append(rail[row][col])
-            col += 1
-        if dir_down:
-            row += 1
-        else:
-            row -= 1
-    ans="".join(result)
+            i=i-1
     return ans
